22ad98c6344ca32d29f26ba096055d7e8454c7a9
hydroakri <hsx011231@gmail.com>
2023-10-06 16:01:25 +0800

    update nvim ibl

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -75,7 +75,9 @@ vim.o.termguicolors = true
 vim.opt.termguicolors = true
 -- 不可见字符的显示，这里只把空格显示为一个点
 vim.opt.list = true
-vim.opt.listchars = { space = " ", tab = "> ", eol = "↵" }
+-- vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:↵")
+vim.opt.listchars:append("tab:▏ ")
 vim.opt.clipboard = unamedplus --see :help clipboard
 vim.opt.cmdheight = 0
 -- 开启 Folding
@@ -88,7 +90,6 @@ vim.o.foldenable = true
 
 -- font
 vim.opt.guifont = { "CaskaydiaCove Nerd Font", ":h12" }
-vim.g.indent_blankline_filetype_exclude = { "dashboard" }
 
 -- ========================================== keybind ====================================
 -- Leader key
@@ -399,8 +400,8 @@ require("lazy").setup({
 						"json",
 						"dockerfile",
 						"cmake",
-                        "make",
-                        "comment",
+						"make",
+						"comment",
 						"diff",
 						"html",
 						"http",
@@ -676,28 +677,32 @@ require("lazy").setup({
 		},
 		{
 			"lukas-reineke/indent-blankline.nvim",
-			config = true,
+			main = "ibl",
 			event = "BufReadPre",
-			opts = {
-				space_char_blankline = " ",
-				show_current_context = true,
-				show_current_context_start = true,
-				char_highlight_list = {
-					"line1",
-					"line2",
-					"line3",
-					"line4",
-					"line5",
-					"line6",
-				},
-			},
-			init = function()
-				vim.cmd([[highlight line1 guifg=#E06C75 gui=nocombine]])
-				vim.cmd([[highlight line2 guifg=#E5C07B gui=nocombine]])
-				vim.cmd([[highlight line3 guifg=#98C379 gui=nocombine]])
-				vim.cmd([[highlight line4 guifg=#56B6C2 gui=nocombine]])
-				vim.cmd([[highlight line5 guifg=#61AFEF gui=nocombine]])
-				vim.cmd([[highlight line6 guifg=#C678DD gui=nocombine]])
+			opts = {},
+			config = function()
+				local highlight = {
+					"RainbowRed",
+					"RainbowYellow",
+					"RainbowBlue",
+					"RainbowOrange",
+					"RainbowGreen",
+					"RainbowViolet",
+					"RainbowCyan",
+				}
+
+				local hooks = require("ibl.hooks")
+				hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+					vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+					vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+					vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+					vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+					vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+					vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+					vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+				end)
+
+				require("ibl").setup({ indent = { highlight = highlight } })
 			end,
 		},
 		{ "petertriho/nvim-scrollbar", config = true, event = "BufReadPost" },
@@ -723,10 +728,10 @@ require("lazy").setup({
 				require("windows").setup()
 			end,
 		},
-        {
-            "LunarVim/bigfile.nvim",
-            event = "BufReadPre",
-        },
+		{
+			"LunarVim/bigfile.nvim",
+			event = "BufReadPre",
+		},
 		{
 			"folke/which-key.nvim",
 			keys = { "<leader>", '"', "'", "`", "c", "v", "g" },
