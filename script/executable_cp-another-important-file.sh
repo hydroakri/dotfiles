a4e537b84f466b7183c117a5617cd083438eb1ff
hydroakri <hydroakri@protonmail.com>
2024-06-23 19:12:48 +0800

    update aif

--- script/executable_cp-another-important-file.sh
+++ script/executable_cp-another-important-file.sh
@@ -9,15 +9,13 @@ cp -r /etc/mkinitcpio.d/ $aifdir/etc/
 cp -r /etc/modprobe.d/ $aifdir/etc/
 cp -r /etc/modules-load.d/ $aifdir/etc/
 cp -r /etc/sysctl.d $aifdir/etc/
-cp /etc/fstab $aifdir/etc/
 cp /etc/environment $aifdir/etc/
 cp /etc/mkinitcpio.conf $aifdir/etc/
-cp /etc/tlp.conf $aifdir/etc/
 doas chmod -R 777 $aifdir
 doas chown -R -c hydroakri:hydroakri $aifdir
 
 systemctl list-units --type=service --state=running > $aifdir/enabled-service.txt
 systemctl list-timers --state=active > $aifdir/active-timers.txt
 pacman -Qqe > $aifdir/pkgs.txt
-flatpak list > $aifdir/flatpak.txt
+flatpak list --columns=app > $aifdir/flatpak.txt
 
