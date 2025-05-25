9b71d970d03d524e969fc896f3d26f9d3790e44c
hydroakri <hydroakri@protonmail.com>
2025-05-25 18:08:42 +0800

    use home-manager to manage dotfiles

--- flake/hydroakri.nix
+++ flake/hydroakri.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  rime-ice = "https://github.com/iDvel/rime-ice";
+  rime-ice-path = "~/.local/share/fcitx5/rime";
+in {
   home.username = "hydroakri";
   home.homeDirectory = "/home/hydroakri";
   imports = [ ];
@@ -7,9 +11,9 @@
     "Xft.dpi" = 96;
   };
   programs.git.enable = true;
-  programs.zsh = {
+  programs.bash = {
     enable = true;
-    initContent = ''
+    bashrcExtra = ''
       if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
       then
         shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
@@ -39,4 +43,38 @@
   ];
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
+
+  # 直接将当前文件夹的配置文件，链接到 Home 目录下的指定位置
+  home.file.".zshrc".source = ../dot_zshrc;
+
+  # 递归将某个文件夹中的文件，链接到 Home 目录下的指定位置
+  home.file.".config" = {
+    source = ../private_dot_config;
+    recursive = true;
+    executable = true;
+  };
+  home.file.".local" = {
+    source = ../dot_local;
+    recursive = true;
+    executable = true;
+  };
+  home.file.".ssh" = {
+    source = ../private_dot_ssh;
+    recursive = true;
+    executable = true;
+  };
+
+  # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+  home.activation.cloneOrPullRepo = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ -d "${rime-ice-path}/.git" ]; then
+      echo "Pulling existing repository at ${rime-ice-path}"
+      git -C "${rime-ice-path}" pull
+    else
+      echo "Cloning repository to ${rime-ice-path}"
+      git clone ${rime-ice} "${rime-ice-path}"
+    fi
+  '';
 }
