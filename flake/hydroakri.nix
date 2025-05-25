3de4248f4c0fb2eb812632c674ab81fa676118a1
hydroakri <24475059+hydroakri@users.noreply.github.com>
2025-05-25 18:56:28 +0800

    use home-manager to manage dotfiles

--- flake/hydroakri.nix
+++ flake/hydroakri.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 let
   rime-ice = "https://github.com/iDvel/rime-ice";
-  rime-ice-path = "~/.local/share/fcitx5/rime";
+  rime-ice-path = ".local/share/fcitx5/rime";
 in {
   home.username = "hydroakri";
   home.homeDirectory = "/home/hydroakri";
@@ -11,16 +11,7 @@ in {
     "Xft.dpi" = 96;
   };
   programs.git.enable = true;
-  programs.bash = {
-    enable = true;
-    bashrcExtra = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
-  };
+  programs.fish.enable = true;
   home.packages = with pkgs; [
     # nix related
     nix-output-monitor
@@ -44,37 +35,51 @@ in {
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 
-  # 直接将当前文件夹的配置文件，链接到 Home 目录下的指定位置
-  home.file.".zshrc".source = ../dot_zshrc;
-
-  # 递归将某个文件夹中的文件，链接到 Home 目录下的指定位置
-  home.file.".config" = {
-    source = ../private_dot_config;
-    recursive = true;
-    executable = true;
-  };
-  home.file.".local" = {
-    source = ../dot_local;
-    recursive = true;
-    executable = true;
-  };
-  home.file.".ssh" = {
-    source = ../private_dot_ssh;
-    recursive = true;
-    executable = true;
+  home.file = {
+    ".zshrc".source = ../dot_zshrc;
+    ".config" = {
+      source = ../private_dot_config;
+      recursive = true;
+      executable = true;
+    };
+    ".local" = {
+      source = ../dot_local;
+      recursive = true;
+      executable = true;
+    };
+    ".ssh" = {
+      source = ../private_dot_ssh;
+      recursive = true;
+      executable = true;
+    };
   };
 
   # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
   # home.file.".xxx".text = ''
-  #     xxx
+  # xxx
   # '';
-  home.activation.cloneOrPullRepo = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [ -d "${rime-ice-path}/.git" ]; then
-      echo "Pulling existing repository at ${rime-ice-path}"
-      git -C "${rime-ice-path}" pull
-    else
-      echo "Cloning repository to ${rime-ice-path}"
-      git clone ${rime-ice} "${rime-ice-path}"
-    fi
-  '';
+  systemd.user.services.gitSyncRime = {
+    Unit = {
+      Description = "Sync Rime repo from GitHub";
+      After = [ "network-online.target" ];
+    };
+
+    Service = {
+      Type = "oneshot";
+      ExecStart = ''
+        if [ -d "${rime-ice-path}/.git" ]; then
+          echo "Pulling existing repository at ${rime-ice-path}"
+          ${pkgs.git}/bin/git -C "${rime-ice-path}" pull
+        else
+          echo "Cloning repository to ${rime-ice-path}"
+          ${pkgs.git}/bin/git clone ${rime-ice} "${rime-ice-path}"
+        fi
+      '';
+    };
+
+    Install = { WantedBy = [ "default.target" ]; };
+  };
+
+  #home.activation.cloneOrPullRepo = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  #'';
 }
