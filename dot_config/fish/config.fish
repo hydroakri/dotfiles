958cccf85cb5dea910d48e112ff2a48703abccb7
hydroakri <24475059+hydroakri@users.noreply.github.com>
2025-05-27 17:05:02 +0800

    add dae as Tproxy, add firewall rules, change flatpak settings, alias zoxide as cd in fish shell, disable amdvlk as driver, enable nvidia persistent.

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,10 +4,10 @@ zoxide init fish | source
 
 set -x fish_key_bindings fish_vi_key_bindings
 
-# 一些个人变量
-set -gx PATH $PATH /home/$USER/script /var/lib/flatpak/exports/bin ~/.local/share/flatpak/exports/bin /home/linuxbrew/.linuxbrew/bin
-set -gx XDG_DATA_DIRS $XDG_DATA_DIRS /var/lib/flatpak/exports/share /home/$USER/.local/share/flatpak/exports/share
 set -gx EDITOR nvim
+
+alias cd=__zoxide_z
+alias cdi=__zoxide_zi
 alias vi='nvim'
 alias tmux='tmux -u'
 alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}" '
