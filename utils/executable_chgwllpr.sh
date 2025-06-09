5268704a5bc33f91f3b4a835ef5126a0cfd87bdf
hydroakri <hydroakri@protonmail.com>
2025-06-09 22:34:47 +0800

    Due to implement of CI to automerdeleted the dnscrypt-proxy utils and other scripts. nixpkgs update.
    Due to the CI I implement in other repo to automerge the blocklist from
    upstream.

--- utils/executable_chgwllpr.sh
+++
@@ -1,9 +0,0 @@
-current_paper=$(find ~/Pictures/wllppr -name '*.*g' | shuf -n1)
-random_lockscreen=$(find ~/Pictures/wllppr -name '*.png' | shuf -n1)
-
-############################ change wallpaper ##############################
-# swww img $current_paper --transition-step 24
-swaybg -i $current_paper -m fill
-
-####################### change hyprlock paper ###############################
-# sed -i "0,/^ *path.*/{s,,    path=$random_lockscreen,}" ~/.config/hypr/hyprlock.conf
