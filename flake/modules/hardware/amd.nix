bddcf25ef3a4ed84fba4733cb2b12b406b67e54e
hydroakri <24475059+hydroakri@users.noreply.github.com>
2026-07-31 10:17:32 +0000

    chore(fix): libdisplay, chezmoi

--- flake/modules/hardware/amd.nix
+++ flake/modules/hardware/amd.nix
@@ -13,8 +13,28 @@
       hardware.graphics.extraPackages32 = [ pkgs.rocmPackages.clr.icd ];
     })
     {
+      nixpkgs.overlays = [
+        (_final: prev: {
+          libdisplay-info_0_3 = prev.libdisplay-info.overrideAttrs (
+            finalAttrs: _: {
+              version = "0.3.0";
+              src = prev.fetchFromGitLab {
+                domain = "gitlab.freedesktop.org";
+                owner = "emersion";
+                repo = "libdisplay-info";
+                rev = finalAttrs.version;
+                sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+              };
+            }
+          );
+        })
+      ];
+
       hardware.amdgpu.overdrive.enable = lib.mkDefault true;
-      services.lact.enable = lib.mkDefault true;
+      services.lact = {
+        enable = lib.mkDefault true;
+        package = pkgs.lact.override { libdisplay-info = pkgs.libdisplay-info_0_3; };
+      };
     }
   ];
 }
